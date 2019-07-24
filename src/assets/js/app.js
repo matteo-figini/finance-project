560948340c7f1887d85a7f669f3c7d9d6ffc8694
Matteo <matteo.figio@gmail.com>
2019-07-24 20:10:36 +0200

    Adjusted JS elements

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,8 +1,17 @@
+var eurExchangeRates;
+
+var usdExchangeRate = 0;
+var gbpExchangeRate = 0;
+var chfExchangeRate = 0;
+
 $(document).ready(function() {
     $.getJSON('https://api.exchangeratesapi.io/latest', function(data) {
-        var conversion = JSON.parse(JSON.stringify(data));
-        $('#home-usd').html("<h4>" + conversion.rates.USD.toLocaleString() + "</h4>");
-        $('#home-gbp').html("<h4>" + conversion.rates.GBP.toLocaleString() + "</h4>");
-        $('#home-chf').html("<h4>" + conversion.rates.CHF.toLocaleString() + "</h4>");
+        eurExchangeRates = JSON.parse(JSON.stringify(data));
+        usdExchangeRate = eurExchangeRates.rates.USD;
+        gbpExchangeRate = eurExchangeRates.rates.GBP;
+        chfExchangeRate = eurExchangeRates.rates.CHF;
+        $('#home-usd').html( String("<h4>" + usdExchangeRate.toLocaleString() + "</h4>"));
+        $('#home-gbp').html( String("<h4>" + gbpExchangeRate.toLocaleString() + "</h4>"));
+        $('#home-chf').html( String("<h4>" + chfExchangeRate.toLocaleString() + "</h4>"));
     });
 });
