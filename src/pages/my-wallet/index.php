fec71db76e3a4cba63166fc663c64fdca02501ed
Matteo <matteo.figio@gmail.com>
2019-07-30 17:25:23 +0200

    H4 Dashboard title

--- src/pages/my-wallet/index.php
+++ src/pages/my-wallet/index.php
@@ -19,7 +19,7 @@
         <div> <?php include '../../partials/top-container.php'; ?> </div>
         <div> <?php include '../../partials/sidebar-menu.php'; ?> </div>
         <div class="w3-overlay w3-hide-large w3-animate-opacity" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
-        <div class="w3-main" style="margin-left:300px;margin-top:43px;">
+        <div class="w3-main" style="margin-left:300px; margin-top:43px;">
             <div> <?php include '../../partials/footer.php'; ?> </div>
         </div>
     </body>
--- src/partials/sidebar-menu.php
+++ src/partials/sidebar-menu.php
@@ -1,6 +1,6 @@
 <nav class="w3-sidebar w3-collapse w3-white w3-animate-left" style="z-index:3;width:300px;" id="mySidebar"><br>
     <div class="w3-container">
-        <h5>Dashboard</h5>
+        <h4>Dashboard</h4>
     </div>
     <div class="w3-bar-block">
         <a href="#" class="w3-bar-item w3-button w3-padding-16 w3-hide-large w3-dark-grey w3-hover-black" onclick="w3_close()" title="close menu"><i class="fa fa-remove fa-fw"></i>  Close Menu</a>
