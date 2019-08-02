e072aa31cceba81bc53f014568120ce329e29596
Matteo <matteo.figio@gmail.com>
2019-08-02 23:32:12 +0200

    Added request for json data in convert currency

--- src/partials/convert-currency.php
+++ src/partials/convert-currency.php
@@ -3,15 +3,22 @@
 </header>
 <div class="w3-container">
     <input type="number" id="valore iniziale" placeholder="1" required>
-    <select class="" name="">
-        <?php
-            $json = file_get_contents("valute.json");
-            $json_data = json_decode($json, true);
-            print_r($json_data);
-
-            // foreach ($json_data as $key => $value) {
-            //
-            // }
-         ?>
-    </select>
+    <select class="w3-select currency-selector" id="select-currency-first"></select>
 </div>
+
+<script type="text/javascript">
+    var xmlhttp = new XMLHttpRequest();
+    var url = "../../data/valute.json";
+
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var jsonResponseData = JSON.parse(this.responseText);
+            // Codice operativo...
+            console.log(jsonResponseData);
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+
+
+</script>
