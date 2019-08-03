38000e1165101d0031664f3bf62e9b86781d7d2c
Matteo <matteo.figio@gmail.com>
2019-08-03 10:43:27 +0200

    Head section under partial file

--- src/pages/watch-stock/index.php
+++ src/pages/watch-stock/index.php
@@ -1,20 +1,8 @@
 <!DOCTYPE html>
 <html lang="it" dir="ltr">
     <head>
-        <meta charset="utf-8">
-        <title>Home - Finance Project</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- Inclusione dei file di scripting -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="../../assets/js/app.js"></script>
-        <script src="../../assets/js/w3.js"></script>
-        <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
-        <!-- Inclusione dei fogli di stile -->
-        <link rel="stylesheet" href="../../assets/css/w3.css">
-        <link rel="stylesheet" href="../../assets/css/app.css">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
-        integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+        <title>Monitora titoli - Finance Project</title>
+        <?php include '../../partials/head-section.php'; ?>
     </head>
     <body>
         <div> <?php include '../../partials/top-container.php'; ?> </div>
